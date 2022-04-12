3f4b56695d9c5e0d4451d118a547494bd68e701e
Tim MacDonald <hello@timacdonald.me>
2022-04-12 11:24:39 +1000

    wip

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -24,6 +24,15 @@ class UserResource extends JsonApiResource
             'posts' => fn () => PostResource::collection($this->posts),
             'comments' => fn () => CommentResource::collection($this->comments),
             'profile_picture' => fn () => ProfilePictureResource::make($this->profilePicture),
+            'random_array' => fn () => ['hello' => 'world'],
+            'non_json_api_resource' => fn () => new class ('tim') extends JsonResource {
+                public function toArray($request)
+                {
+                    return [
+                        'name' => $this->resource,
+                    ];
+                }
+            },
         ];
     }
 }
